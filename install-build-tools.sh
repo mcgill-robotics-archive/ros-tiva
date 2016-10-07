d2e96aac972c117e5fd161cf59d42a50c269f7ab
Steven Dahdah <steven.nfd+gh@gmail.com>
2016-10-07 14:04:29 -0400

    Merge scripts into ne install file, add environment variabled

---
+++ install-build-tools.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Install GNU ARM Toolchain
+sudo add-apt-repository ppa:terry.guo/gcc-arm-embedded
+sudo apt-get update
+sudo apt-get install gcc-arm-none-eabi
+
+# Install dfu-util
+sudo apt-get install dfu-util
+
+# Install lm4flash
+git clone https://github.com/utzig/lm4tools.git
+cd lm4tools/lm4flash
+make
+sudo cp lm4flash /usr/local/bin
+cd ../..
+
+# Copy over Tiva Launchpad udev rules
+sudo cp 99-tiva.rules /etc/udev/rules.d/99-tiva.rules
+
+# Set up rosserial_tivac environment variables
+echo "export TIVA_WARE_PATH=/opt/tivaware" >> ~/.zshrc
+echo "export TIVA_FLASH_EXECUTABLE=lm4flash" >> ~/.zshrc
