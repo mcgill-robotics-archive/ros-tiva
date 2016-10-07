732cb587aecf263c791092501bb91641fe749bfc
Steven Dahdah <steven.nfd+gh@gmail.com>
2016-10-07 12:23:32 -0400

    Update udev rules for tiva launchpad

--- lm4flash.sh
+++ lm4flash.sh
@@ -4,6 +4,3 @@ git clone https://github.com/utzig/lm4tools.git
 cd lm4tools/lm4flash
 make
 sudo cp lm4flash /usr/local/bin
-
-echo 'ATTRS{idVendor}=="1cbe", ATTRS{idProduct}=="00fd", GROUP="users", MODE="0660"' | \
-sudo tee /etc/udev/rules.d/99-stellaris-launchpad.rules
---
+++ udev-rules.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo ln -sr 99-tiva.rules /etc/udev/rules.d/99-tiva.rules
